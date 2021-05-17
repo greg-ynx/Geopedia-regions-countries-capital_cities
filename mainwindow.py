a7569381ee249873984e86e88d8f1768911343ac
Grégory Ployart <gregory.ployart@gmail.com>
2021-05-17 16:43:44 +0200

    Change mainwindow.py structure and Created list_of_countries.py

--- mainwindow.py
+++ mainwindow.py
@@ -17,7 +17,8 @@ class Ui_MainWindow(object):
         self.gridLayout_main.setObjectName("gridLayout_main")
         self.tabWidget = QtWidgets.QTabWidget(self.gridLayoutWidget)
         self.tabWidget.setObjectName("tabWidget")
-# Tab "List of countries"#################################################################################################
+
+# Tab "List of countries"################################################################################################
         self.tab_LOC = QtWidgets.QWidget()
         self.tab_LOC.setObjectName("tab_LOC")
 
@@ -39,6 +40,7 @@ class Ui_MainWindow(object):
         self.verticalLayout_LOC.addWidget(self.comboBox_LOC)
         # END Vertical Layout Widget LOC 1
 
+        # Scroll Area Widget LOC
         self.scrollArea_LOC = QtWidgets.QScrollArea(self.tab_LOC)
         self.scrollArea_LOC.setGeometry(QtCore.QRect(10, 160, 751, 341))
         self.scrollArea_LOC.setWidgetResizable(True)
@@ -56,40 +58,55 @@ class Ui_MainWindow(object):
         self.tableView_LOC.setObjectName("tableView_LOC")
         self.gridLayout_LOC.addWidget(self.tableView_LOC, 0, 0, 1, 1)
         self.scrollArea_LOC.setWidget(self.scrollAreaWidgetContents)
+        #END Scroll Area Widget LOC
+
+        # Search button LOC
         self.search_button_LOC = QtWidgets.QPushButton(self.tab_LOC)
         self.search_button_LOC.setGeometry(QtCore.QRect(560, 70, 75, 23))
         self.search_button_LOC.setObjectName("search_button_LOC")
+        #END search button LOC
+
         self.tabWidget.addTab(self.tab_LOC, "")
+#END Tab "List of countries"#############################################################################################
+
+# Tab "Search Location"##################################################################################################
         self.tab_SO = QtWidgets.QWidget()
         self.tab_SO.setObjectName("tab_SO")
+
         self.verticalLayoutWidget = QtWidgets.QWidget(self.tab_SO)
         self.verticalLayoutWidget.setGeometry(QtCore.QRect(211, 91, 361, 111))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
         self.verticalLayout_SO_1 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout_SO_1.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout_SO_1.setObjectName("verticalLayout_SO_1")
+
         self.label_SO = QtWidgets.QLabel(self.verticalLayoutWidget)
         self.label_SO.setAlignment(QtCore.Qt.AlignCenter)
         self.label_SO.setObjectName("label_SO")
         self.verticalLayout_SO_1.addWidget(self.label_SO)
+
         self.comboBox_SO_Continent = QtWidgets.QComboBox(self.verticalLayoutWidget)
         self.comboBox_SO_Continent.setObjectName("comboBox_SO_Continent")
         self.verticalLayout_SO_1.addWidget(self.comboBox_SO_Continent)
+
         self.comboBox_SO_Country = QtWidgets.QComboBox(self.verticalLayoutWidget)
         self.comboBox_SO_Country.setObjectName("comboBox_SO_Country")
         self.verticalLayout_SO_1.addWidget(self.comboBox_SO_Country)
+
         self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.tab_SO)
         self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(150, 150, 61, 51))
         self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
         self.verticalLayout_SO_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
         self.verticalLayout_SO_2.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout_SO_2.setObjectName("verticalLayout_SO_2")
+
         self.label_SO_Continent = QtWidgets.QLabel(self.verticalLayoutWidget_2)
         self.label_SO_Continent.setObjectName("label_SO_Continent")
         self.verticalLayout_SO_2.addWidget(self.label_SO_Continent)
         self.label_SO_Country = QtWidgets.QLabel(self.verticalLayoutWidget_2)
         self.label_SO_Country.setObjectName("label_SO_Country")
         self.verticalLayout_SO_2.addWidget(self.label_SO_Country)
+
         self.search_button_SO = QtWidgets.QPushButton(self.tab_SO)
         self.search_button_SO.setGeometry(QtCore.QRect(570, 230, 75, 23))
         self.search_button_SO.setObjectName("search_button_SO")
@@ -97,7 +114,10 @@ class Ui_MainWindow(object):
         self.label_SO_research_return.setGeometry(QtCore.QRect(130, 290, 511, 131))
         self.label_SO_research_return.setAlignment(QtCore.Qt.AlignCenter)
         self.label_SO_research_return.setObjectName("label_SO_research_return")
+
         self.tabWidget.addTab(self.tab_SO, "")
+#END Tab "Search Location"###############################################################################################
+
         self.gridLayout_main.addWidget(self.tabWidget, 0, 1, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
